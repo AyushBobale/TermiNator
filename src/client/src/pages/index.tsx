a20ab4bedb98f17c3451c9e5a3fe98dc12625459
Ayush Bobale <bobaleayush@gmail.com>
2023-04-25 12:41:47 +0530

    fonts done

--- src/client/src/pages/index.tsx
+++ src/client/src/pages/index.tsx
@@ -7,17 +7,19 @@ export default function Home() {
       <Head>
         <title>TermINator</title>
       </Head>
-      <div className={styles.container}>
+      <div className={`${styles.container} ${""}`}>
         <div className={styles.console}>
           <div className={styles.consoleHeader}>Header</div>
           <div className={styles.consoleBody}>
-            ls <br />
-            Mandatory arguments to long options are mandatory for short options
-            too. <br />
-            -a, --all do not ignore entries starting with . <br />
-            -A, --almost-all do not list implied . and .. <br />
-            --author with -l, print the author of each file <br />
-            -b, --escape print C-style escapes for nongraphic characters
+            <p>ls</p>
+            <p>
+              Mandatory arguments to long options are mandatory for short
+              options too.
+            </p>
+            <p>-a, --all do not ignore entries starting with . </p>
+            <p>-A, --almost-all do not list implied . and .. </p>
+            <p>--author with -l, print the author of each file </p>
+            <p>-b, --escape print C-style escapes for nongraphic characters</p>
           </div>
         </div>
       </div>
